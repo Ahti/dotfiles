632ef3169d7bab69c96b43ca04ee6b6ccaa9c249
Lukas Stabe <lukas@stabe.de>
2014-12-18 21:12:37 +0100

    fixed right prompt

--- home/.config/fish/functions/fish_right_prompt.fish
+++ home/.config/fish/functions/fish_right_prompt.fish
@@ -3,17 +3,13 @@ set right_arrow \uE0B1
 set segment_separator_rev \uE0B2
 set left_arrow \uE0B3
 
-function __update_right_promt --on-event __update_right_promt
-  commandline -f repaint
-end
-
 function fish_right_prompt
   set_color -b normal
   set_color blue
   echo -n " $segment_separator_rev"
   set_color -b blue
   set_color black
-  echo -n " "(date +"%T")" "#$segment_separator_rev
+  echo -n " "(date +"%T")" " #$segment_separator_rev
   set_color -b normal
   set_color normal
 end
