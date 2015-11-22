c7f692d8b0a8ca1c6374a2f3fd808f8c4e62492b
Lukas Stabe <lukas@stabe.de>
2015-11-22 03:52:23 +0100

    make fancy greeting degrade gracefully

--- home/.config/fish/functions/fish_greeting.fish
+++ home/.config/fish/functions/fish_greeting.fish
@@ -1,7 +1,12 @@
 function fish_greeting
-	set -l cow_dir /usr/local/opt/cowsay/share/cows/
-	set -l cow_list (ls $cow_dir)
+  set -l cow_dir /usr/local/opt/cowsay/share/cows/
+  if begin which fortune >/dev/null
+       and which cowsay >/dev/null
+       and test -d $cow_dir
+       end
+    set -l cow_list (ls $cow_dir)
     set -l cow $cow_list[(math (random) \% (count $cow_list) + 1)]
-	fortune -a | cowsay -f $cow
-    echo $fish_greeting
+    fortune -a | cowsay -f $cow
+  end
+  echo $fish_greeting
 end
