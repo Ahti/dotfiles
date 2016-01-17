3ed0e85335a4dd890927a6906ea5402d53e08de5
Lukas Stabe <lukas@stabe.de>
2016-01-17 05:51:03 +0100

    finalize vim config and display user in prompt

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,7 @@
+# these are here so they are synced with my dotfiles
+# so they do not need to be set on every machine
+set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
+set -x NVIM_TUI_ENABLE_CURSOR_SHAPE 1
+
+set -g theme_display_user yes
+set -g default_user lukas
