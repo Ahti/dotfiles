6c7651dffcf97440dc99d8bd94153c7b67b9d486
Lukas Stabe <lukas@stabe.de>
2017-02-15 02:06:09 +0000

    add cargo bindir to path if it exists

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -11,3 +11,7 @@ if uname -r | grep ARCH >/dev/null
     # use paths from /etc/profile.d
     env -i HOME=$HOME sh -l -c 'export -p' | sed -e "/PATH/s/\"//g;/PATH/s/:/ /g;s/=/ /;s/^export/set -x/;/^set -x PATH /!d" | source
 end
+
+if test -d ~/.cargo/bin
+    set PATH $PATH ~/.cargo/bin
+end
